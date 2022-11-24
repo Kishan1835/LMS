90856485cb803dacc5b1d370ede5bacdc3fcc5c2
Kishan <sharmilakishan.2003@gmail.com>
2022-11-24 20:06:25 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-LIBRARY</title>
+    <link rel="stylesheet" href="css/design.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sono:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css">
+</head>
+
+<body>
+    <section class="header">
+        <Nav>
+            <h1>SNAP LIBRARY</h1>
+            <br>
+            <a href="index.html"><img src="project/logooo.png" alt=""></a>
+            <div class="nav-links" id="navLinks">
+
+            </div>
+            <div class="nav-links">
+                <i class="fa fa-times" onclick="hideMenu()"></i>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="aboutus.php">HumApkeHainKon</a></li>
+                    <li><a href="#">EXPLORE</a></li>
+                </ul>
+            </div>
+            <i class="fa fa-bars" onclick="showMenu()"></i>
+        </Nav>
+        <div class="textbox">
+            <h1>SNAP <br> LIBRARY</h1>
+            <P>AAIYEEEE APKA INTEZAAR THA</P>
+            <a href="register_form.php" class="button">LOGIN/REGISTER</a>
+        </div>
+    </section>
+    <script>
+        var navLinks = document.getElementById("navLinks");
+
+        function showMenu() {
+            navLinks.style.right = "0";
+        }
+
+        function hideMenu() {
+            navLinks.style.right = "-200px";
+        }
+    </script>
+</body>
+
+</html>
---
+++ user.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-LIBRARY</title> 
+    <link rel="stylesheet" href="css/design.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Sono:wght@700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css">
+</head>
+<body>
+<section class="book1"> 
+    <h2>OPERATING SYSTEM</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
+<div class="row1">
+<div class="book1-col">
+<h3>book 1</h3>
+<p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
+</div>
+<div class="book1-col">
+    <h3>book 2</h3>
+    <p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
+    </div>
+    <div class="book1-col">
+        <h3>book 3</h3>
+        <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
+        </div>
+        <div class="book1-col">
+            <h3>book 4</h3>
+            <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+            </div>
+</div>
+</section>
+<section class="book2"> 
+    <h2>COMPUTER GRAPHICS</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
+<div class="row2">
+<div class="book2-col">
+<h3>book 1</h3>
+<p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
+</div>
+<div class="book2-col">
+    <h3>book 2</h3>
+    <p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
+    </div>
+    <div class="book2-col">
+        <h3>book 3</h3>
+        <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
+        </div>
+        <div class="book2-col">
+            <h3>book 4</h3>
+            <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+            </div>
+</div>
+</section>
+</section>
+<section class="book3"> 
+<h2>COMPUTER SYSTEM ARCHITECTURE</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
+<div class="row3">
+<div class="book3-col">
+<h3>book 1</h3>
+<p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
+</div>
+<div class="book3-col">
+<h3>book 2</h3>
+<p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
+</div>
+<div class="book3-col">
+    <h3>book 3</h3>
+    <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
+    </div>
+    <div class="book3-col">
+        <h3>book 4</h3>
+        <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+        </div>
+</div>
+</section>
+</section>
+<section class="book4"> 
+<h2>DATABASE MANAGEMENT SYSTEM</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
+<div class="row4">
+<div class="book4-col">
+<h3>book 1</h3>
+<p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
+</div>
+<div class="book4-col">
+<h3>book 2</h3>
+<p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
+</div>
+<div class="book4-col">
+    <h3>book 3</h3>
+    <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
+    </div>
+    <div class="book4-col">
+        <h3>book 4</h3>
+        <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+        </div>
+</div>
+</section>
+</body>
+</html>
