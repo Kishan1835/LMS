8e8c2a64979a919751690f9cf20cfd0efeb9d88d
Kishan <sharmilakishan.2003@gmail.com>
2022-11-28 13:34:25 +0530

    addesd new files foe about us and is immage

--- index.php
+++ index.php
@@ -6,49 +6,137 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-LIBRARY</title>
-    <link rel="stylesheet" href="css/design.css">
+    <link rel="stylesheet" href="css/design1.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Sono:wght@700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css">
+    <script src="https://kit.fontawesome.com/85844ff574.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <section class="header">
         <Nav>
-            <h1>SNAP LIBRARY</h1>
-            <br>
-            <a href="index.html"><img src="project/logooo.png" alt=""></a>
+            <a href="index.html"><img src="" alt=""></a>
             <div class="nav-links" id="navLinks">
 
             </div>
             <div class="nav-links">
                 <i class="fa fa-times" onclick="hideMenu()"></i>
                 <ul>
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="about_us.php">HumApkeHainKon</a></li>
-                    <li><a href="#">EXPLORE</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="aboutuss.php">HumApkeHainKon</a></li>
+                    <li><a href="#"></a></li>
                 </ul>
             </div>
-            <i class="fa fa-bars" onclick="showMenu()"></i>
+            <i class="fa-solid fa-bars" onclick="showMenu()"></i>
         </Nav>
         <div class="textbox">
             <h1>SNAP <br> LIBRARY</h1>
-            <P>AAIYEEEE APKA INTEZAAR THA</P>
-            <a href="register_form.php" class="button">LOGIN/REGISTER</a>
+            <P>AAIYEEEE AAPKA INTEZAAR THAAA </P>
+            <a href="register_form.php" class="button">Login/Register</a>
         </div>
     </section>
-    <script>
-        var navLinks = document.getElementById("navLinks");
+    <!-- user page -->
+    <!-- <section class="book1"> 
+        <h2>OPERATING SYSTEM</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
+<div class="row1">
+    <div class="book1-col">
+    <h3>book 1</h3>
+    <p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
+    </div>
+    <div class="book1-col">
+        <h3>book 2</h3>
+        <p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
+        </div>
+        <div class="book1-col">
+            <h3>book 3</h3>
+            <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
+            </div>
+            <div class="book1-col">
+                <h3>book 4</h3>
+                <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+                </div>
+</div>
+    </section>
+    <section class="book2"> 
+        <h2>COMPUTER GRAPHICS</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
+<div class="row2">
+    <div class="book2-col">
+    <h3>book 1</h3>
+    <p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
+    </div>
+    <div class="book2-col">
+        <h3>book 2</h3>
+        <p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
+        </div>
+        <div class="book2-col">
+            <h3>book 3</h3>
+            <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
+            </div>
+            <div class="book2-col">
+                <h3>book 4</h3>
+                <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+                </div>
+</div>
+    </section>
+</section>
+<section class="book3"> 
+    <h2>COMPUTER SYSTEM ARCHITECTURE</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
+<div class="row3">
+<div class="book3-col">
+<h3>book 1</h3>
+<p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
+</div>
+<div class="book3-col">
+    <h3>book 2</h3>
+    <p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
+    </div>
+    <div class="book3-col">
+        <h3>book 3</h3>
+        <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
+        </div>
+        <div class="book3-col">
+            <h3>book 4</h3>
+            <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+            </div>
+</div>
+</section>
+</section>
+<section class="book4"> 
+    <h2>DATABASE MANAGEMENT SYSTEM</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
+<div class="row4">
+<div class="book4-col">
+<h3>book 1</h3>
+<p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
+</div>
+<div class="book4-col">
+    <h3>book 2</h3>
+    <p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
+    </div>
+    <div class="book4-col">
+        <h3>book 3</h3>
+        <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
+        </div>
+        <div class="book4-col">
+            <h3>book 4</h3>
+            <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+            </div>
+</div>
+</section>
 
-        function showMenu() {
+    <script>
+        var navLinks=document.getElementById("navLinks");
+        function showMenu(){
             navLinks.style.right = "0";
         }
-
-        function hideMenu() {
-            navLinks.style.right = "-200px";
+        function hideMenu(){
+            navLinks.style.right ="-200px";
         }
-    </script>
+    </script> -->
 </body>
 
 </html>
--- user.php
+++ user.php
@@ -1,106 +1,110 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-LIBRARY</title> 
-    <link rel="stylesheet" href="css/design.css">
+    <title>E-LIBRARY</title>
+    <link rel="stylesheet" href="css/design1.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Sono:wght@700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sono:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css">
 </head>
+
 <body>
-<section class="book1"> 
-    <h2>OPERATING SYSTEM</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
-<div class="row1">
-<div class="book1-col">
-<h3>book 1</h3>
-<p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
-</div>
-<div class="book1-col">
-    <h3>book 2</h3>
-    <p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
-    </div>
-    <div class="book1-col">
-        <h3>book 3</h3>
-        <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
-        </div>
-        <div class="book1-col">
-            <h3>book 4</h3>
-            <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+    </section>
+    <section class="book1">
+        <h2>OPERATING SYSTEM</h2>
+        <p class="abc">An OS is the program that, after being initially loaded into the computer by a boot program, manages all of the other application programs in a computer. </p>
+        <div class="row1">
+            <div class="book1-col">
+                <h3>Operating System <br> Concepts</h3>
+                <p> <a href="https://drive.uqu.edu.sa/_/mskhayat/files/MySubjects/2017SS%20Operating%20Systems/Abraham%20Silberschatz-Operating%20System%20Concepts%20(9th,2012_12).pdf"> CLICK HERE TO OPEN PDF </a></p>
             </div>
-</div>
-</section>
-<section class="book2"> 
-    <h2>COMPUTER GRAPHICS</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
-<div class="row2">
-<div class="book2-col">
-<h3>book 1</h3>
-<p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
-</div>
-<div class="book2-col">
-    <h3>book 2</h3>
-    <p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
-    </div>
-    <div class="book2-col">
-        <h3>book 3</h3>
-        <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
-        </div>
-        <div class="book2-col">
-            <h3>book 4</h3>
-            <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+            <div class="book1-col">
+                <h3>OS - Internal and Design Principles</h3>
+                <p> <a href="https://www.academia.edu/44459131/William_Stallings_Operating_SystemsInternals_and_Design_Principles_7th_Edition_"> CLICK HERE TO OPEN PDF </a></p>
             </div>
-</div>
-</section>
-</section>
-<section class="book3"> 
-<h2>COMPUTER SYSTEM ARCHITECTURE</h2>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
-<div class="row3">
-<div class="book3-col">
-<h3>book 1</h3>
-<p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
-</div>
-<div class="book3-col">
-<h3>book 2</h3>
-<p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
-</div>
-<div class="book3-col">
-    <h3>book 3</h3>
-    <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
-    </div>
-    <div class="book3-col">
-        <h3>book 4</h3>
-        <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+            <div class="book1-col">
+                <h3>OS - Design and Implementation</h3>
+                <p> <a href="https://csc-knu.github.io/sys-prog/books/Andrew%20S.%20Tanenbaum%20-%20Operating%20Systems.%20Design%20and%20Implementation.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+            <div class="book1-col">
+                <h3>Revision notes for <br> practice</h3>
+                <p> <a href="https://www.studocu.com/in/document/bangalore-university/bachelor-of-computer-applications/operating-system-notes-for-3-sem-bca/14364724">CLICK HERE TO OPEN PDF</a></p>
+            </div>
         </div>
-</div>
-</section>
-</section>
-<section class="book4"> 
-<h2>DATABASE MANAGEMENT SYSTEM</h2>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, neque?</p>
-<div class="row4">
-<div class="book4-col">
-<h3>book 1</h3>
-<p>youtube link Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ratione.</p>
-</div>
-<div class="book4-col">
-<h3>book 2</h3>
-<p>youtube link Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, distinctio.</p>
-</div>
-<div class="book4-col">
-    <h3>book 3</h3>
-    <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.</p>
-    </div>
-    <div class="book4-col">
-        <h3>book 4</h3>
-        <p>youtube link Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est.</p>
+    </section>
+    <section class="book2">
+        <h2>DATA STRUCTURES USING C</h2>
+        <p class="abc">Data Structures are typically used to organize, process, retrieve and store data on computers for efficient use. They help in understanding the nature of a given problem at a deeper level.</p>
+        <div class="row2">
+            <div class="book2-col">
+                <h3>Data Structures and <br> Algorithms in C</h3>
+                <p> <a href="https://itlectures.ro/wp-content/uploads/2016/04/AdamDrozdek__DataStructures_and_Algorithms_in_C_4Ed.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+            <div class="book2-col">
+                <h3>Algorithms and <br> Applications in C</h3>
+                <p> <a href="https://o6ucs.files.wordpress.com/2012/10/data-structures-algorithms-and-applications-in-c-by-sartraj-sahani.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+            <div class="book2-col">
+                <h3>Research and <br> Development</h3>
+                <p> <a href="https://csit.ust.edu.sd/files/2019/10/Data-structure-using-C-1.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+            <div class="book2-col">
+                <h3>Revision notes for <br> practice</h3>
+                <p> <a href="http://www.vijayacollege.ac.in/Content/PDF/BCA-data-structures-notes.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
         </div>
-</div>
-</section>
+    </section>
+    </section>
+    <section class="book3">
+        <h2>DATABASE MANAGEMENT SYSTEM</h2>
+        <p class="abc">In computing, a database is an organized collection of data stored and accessed electronically. Small databases can be stored on a file system, while large databases are hosted on computer clusters or cloud storage.</p>
+        <div class="row3">
+            <div class="book3-col">
+                <h3>Fundamentals of <br> Database System</h3>
+                <p> <a href="https://asolanki.co.in/wp-content/uploads/2019/02/Fundamentals_of_Database_Systems_6th_Edition-1.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+            <div class="book3-col">
+                <h3>Introduction to <br> Database System</h3>
+                <p> <a href="https://lc.fie.umich.mx/~rodrigo/BD/An%20Introduction%20to%20Database%20Systems%208e%20By%20C%20J%20Date.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+            <div class="book3-col">
+                <h3>Introduction to <br> Management System</h3>
+                <p> <a href="https://spectrum.library.concordia.ca/988586/1/An-Introduction-to-Database-Systems-Bipin-C.DESAI.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+            <div class="book3-col">
+                <h3>Revision notes <br> for practice</h3>
+                <p> <a href="https://www.studocu.com/in/document/university-of-mysore/bca/dbms-complete-notes-bca/3406810">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+        </div>
+    </section>
+    </section>
+    <section class="book4">
+        <h2>COMPUTER SYSTEM ARCHITECTURE</h2>
+        <p class="abc">In computer engineering, computer architecture is a description of the structure of a computer system made from component parts. It can sometimes be a high-level description that ignores details of the implementation. </p>
+        <div class="row4">
+            <div class="book4-col">
+                <h3>Computer System Architecture</h3>
+                <p> <a href="https://faculty.tarleton.edu/agapie/documents/cs_343_arch/01_Ch1_Ch2.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+            <div class="book4-col">
+                <h3>Structured Computer Organization</h3>
+                <p> <a href="https://csc-knu.github.io/sys-prog/books/Andrew%20S.%20Tanenbaum%20-%20Structured%20Computer%20Organization.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+            <div class="book4-col">
+                <h3>Computer Organization Design</h3>
+                <p> <a href="https://ict.iitk.ac.in/wp-content/uploads/CS422-Computer-Architecture-ComputerOrganizationAndDesign5thEdition2014.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+            <div class="book4-col">
+                <h3>Revision notes <br> for practice</h3>
+                <p> <a href="http://mdudde.net/books/BCA/bca%20-%20III%20Sem/bca-201-3rd-Computer%20System%20Architecture.pdf">CLICK HERE TO OPEN PDF</a></p>
+            </div>
+        </div>
+    </section>
 </body>
+
 </html>
